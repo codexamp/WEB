d25d8fc70ae8973f8f88de1f8b1b728fa924a596
codexamp <74566444+codexamp@users.noreply.github.com>
2021-01-26 19:17:12 +0200

    Finaly fix

--- Copy_button/copy.js
+++ Copy_button/copy.js
@@ -1,14 +1,8 @@
 let copyBtn = document.querySelector('.copy-btn');
-let text = document.querySelector('.text').innerText;
+let text = document.querySelector('.text-copy').innerText;
 let message = document.querySelector('.copy-message');
 
 
 copyBtn.addEventListener('click', function() {
   navigator.clipboard.writeText(text);
-  setTimeout(function(){
-    message.style.display = 'flex'
-  }, 100)
-  setTimeout(function(){
-    message.style.display = 'none'
-  }, 1000)
 });
